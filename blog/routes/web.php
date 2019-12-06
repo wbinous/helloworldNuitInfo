e48498e4f14d619ebeaf6f86d63718b99bcf6c1e
Wassim <wbinous@gmail.com>
2019-12-06 06:41:22 +0100

    modifs routes

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -15,16 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/connexion', 'ConnexionController@formulaire');
 Route::post('/connexion', 'ConnexionController@traitement');
 
 Route::get('/inscription', 'ConnexionController@inscrireFormulaire');
 Route::post('/inscription', 'ConnexionController@inscrireUtilisateur');
-=======
-Route::get('/inscription', 'InscriptionController@afficherFormulaire');
-Route::post('/inscription', 'InscriptionController@traiterFormulaire');
-
 
 Route::view('/article/alimentaire','/article/alimentaire');
 Route::view('/article/associations','/article/associations');
@@ -33,4 +28,3 @@ Route::view('/article/logement','/article/logement');
 Route::view('/article/transport','/article/transport');
 Route::view('/article/loisirs','/article/loisirs');
 Route::view('/article/sante','/article/sante');
->>>>>>> 0d833ccaaba39263e4b9c26f880ab372eeae06c9
